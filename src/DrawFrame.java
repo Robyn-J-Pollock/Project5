432db7c9e81d8aabe9cf1fb0a378c8f641e3a2ef
Robyn Pollock <pollock.robyn.j@gmail.com>
2019-11-28 22:37:58 -0600

    Created Blank Working Frame

--- src/DrawFrame.java
+++ src/DrawFrame.java
@@ -1,3 +1,7 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 /*
  * @author Robyn Pollock
  * @version 11/28/2019
@@ -6,6 +10,31 @@
  * Creates a gui that allows the user to select a hamming distance.
  * 	Finds the stations that fall within that hamming distance.
  */
-public class DrawFrame {
+public class DrawFrame extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public DrawFrame() {
+		//sets title
+		super("Hamming Distance");
+		
+		//sets the layout manager
+		this.setLayout(new BorderLayout());
+		//TODO: Add components
+		
+		//setup frame
+		this.setSize(800, 600);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		DrawFrame frame = new DrawFrame();
+		frame.isActive();
+	}
 }
