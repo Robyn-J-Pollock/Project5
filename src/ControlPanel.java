7dff3fb020070291c8ce1c9ceef5e0a1b081c59f
Robyn Pollock <pollock.robyn.j@gmail.com>
2019-11-28 23:27:32 -0600

    Added all components and buttons

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,3 +1,4 @@
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
@@ -19,8 +20,6 @@ public class ControlPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	protected JPanel actionPanel;
-	
 	protected JSlider hamDistSlider;
 	
 	protected JButton showStationButton, calcHDButton, addStationButton;
@@ -31,9 +30,35 @@ public class ControlPanel extends JPanel {
 	
 	protected JTextField[] textDistArray;
 	
+	protected JPanel bottomPanel;
 	
 	
-	public ControlPanel() {
+	public ControlPanel() {		
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
+		hamDistSlider = new JSlider();
+		showStationButton = new JButton();
+		calcHDButton = new JButton();
+		addStationButton = new JButton();
+		showStationArea = new JTextArea();
+		compareWithBox = new JComboBox<String>();
+		textDistArray = new JTextField[7];
+		for (int x = 0; x < 7; x++)
+			textDistArray[x] = new JTextField();
+		bottomPanel = new JPanel();
+		
+		this.add(textDistArray[0]);
+		this.add(hamDistSlider);
+		this.add(showStationButton);
+		this.add(showStationArea);
+		this.add(compareWithBox);
+		this.add(calcHDButton);
+		for (int x = 1; x < 6; x++)
+			this.add(textDistArray[x]);
+		
+		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+		bottomPanel.add(addStationButton);
+		bottomPanel.add(textDistArray[6]);
+		this.add(bottomPanel);
 	}
 }
