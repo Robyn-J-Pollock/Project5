7e9d7a32fc60fd0cc013744136d1788f986a058d
Robyn Pollock <pollock.robyn.j@gmail.com>
2019-11-28 23:09:45 -0600

    Created protected JComponents

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Project5
 Task 1: Create base frame for project. Use DrawFrame.java
 Task 2: Create Panel for project. Create new class ControlPanel
+Task 3: Create JSlider, JButton x 3, JTextArea, JComboBox, JTextField x6 in ControlPanel.java 
--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,4 +1,9 @@
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 /*
  * @author Robyn Pollock
  * @version 11/28/2019
@@ -13,6 +18,20 @@ public class ControlPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	protected JPanel actionPanel;
+	
+	protected JSlider hamDistSlider;
+	
+	protected JButton showStationButton, calcHDButton, addStationButton;
+	
+	protected JTextArea showStationArea;
+	
+	protected JComboBox<String> compareWithBox;
+	
+	protected JTextField[] textDistArray;
+	
+	
 	
 	public ControlPanel() {
 		
